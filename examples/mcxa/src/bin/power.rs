c64f976e7e30fc1a68ac5567b8145fcbd693a2bd
James Munns <james@onevariable.com>
2026-01-22 15:54:30 +0100

    Try switching to the 16Kosc as system clock
    The results for this are interesting, the power is roughly 1.65mA baseline,
    with spikes up to 1.8 to 1.9mA.
    
    However, the spikes are not exactly correlated with the code itself: the
    spikes are at roughly 10Hz, and I don't see a discernable change for either
    the 250ms wakeups or the 100ms busy loop. That being said: The LED is still
    correctly oscillating at 1/2Hz, confirmed with the scope trace.
    
    It's possible this is something else on the board, or a quirk of the internal
    DC/DC?

--- examples/mcxa/src/bin/power.rs
+++ examples/mcxa/src/bin/power.rs
@@ -2,19 +2,35 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Instant, Ticker, Timer};
+use embassy_mcxa::clocks::{PoweredClock, config::{Div8, Fro16KConfig, MainClockConfig, MainClockSource}};
+use embassy_time::{Duration, Instant, Ticker};
 use hal::gpio::{DriveStrength, Level, Output, SlewRate};
 use {defmt_rtt as _, embassy_mcxa as hal, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = hal::init(hal::config::Config::default());
+    let mut cfg = hal::config::Config::default();
+    cfg.clock_cfg.firc = None;
+    cfg.clock_cfg.sirc.fro_12m_enabled = false;
+    cfg.clock_cfg.sirc.fro_lf_div = None;
+    let mut fro = Fro16KConfig::default();
+    fro.vsys_domain_active = true;
+    fro.vdd_core_domain_active = true;
+    cfg.clock_cfg.fro16k = Some(fro);
+    cfg.clock_cfg.sosc = None;
+    cfg.clock_cfg.spll = None;
+    cfg.clock_cfg.main_clock = MainClockConfig {
+        source: MainClockSource::RoscFro16K,
+        power: PoweredClock::NormalEnabledDeepSleepDisabled,
+        ahb_clk_div: Div8::no_div(),
+    };
+    let p = hal::init(cfg);
 
     defmt::info!("Blink example");
 
     let mut red = Output::new(p.P3_18, Level::High, DriveStrength::Normal, SlewRate::Fast);
-    let mut green = Output::new(p.P3_19, Level::High, DriveStrength::Normal, SlewRate::Fast);
-    let mut blue = Output::new(p.P3_21, Level::High, DriveStrength::Normal, SlewRate::Fast);
+    // let mut green = Output::new(p.P3_19, Level::High, DriveStrength::Normal, SlewRate::Fast);
+    // let mut blue = Output::new(p.P3_21, Level::High, DriveStrength::Normal, SlewRate::Fast);
 
     let mut ticker = Ticker::every(Duration::from_millis(250));
 
